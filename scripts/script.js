5369d2ad3ed42ab11f99d53c3e839d5a2e561464
Kyler Nelson <kylern@utahcounty.gov>
2021-01-06 16:49:04 -0700

    Add demo sidecar script and add npm launch scripts

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+console.log('Hello World!');
+console.log(`Hello from the ${process.env.NODE_ENV} environment`);
+console.log(`Hello from ${process.env.MESSAGE}`);
+console.log(process.env.MESSAGE2);
