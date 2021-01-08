1121eedec113f9adb0a050560449f83ec990c4af
Kyler Nelson <kylern@utahcounty.gov>
2021-01-08 16:40:53 -0700

    Update script.js to have a variable

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-console.log('Hello World!');
+helloMessage = 'Hello World!';
+console.log(helloMessage);
 console.log(`Hello from the ${process.env.NODE_ENV} environment`);
 console.log(`Hello from ${process.env.MESSAGE}`);
 console.log(process.env.MESSAGE2);
