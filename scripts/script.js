ecba5e1970d8914813f972ce4a2f450c413d38e5
Kyler Nelson <kylern@utahcounty.gov>
2021-01-11 11:43:58 -0700

    Add demo function to script

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
 helloMessage = 'Hello World!';
 console.log(helloMessage);
 console.log(`Hello from the ${process.env.NODE_ENV} environment`);
+helloMessage = 'New Message';
+printNTimes(helloMessage, 3);
 console.log(`Hello from ${process.env.MESSAGE}`);
 console.log(process.env.MESSAGE2);
+
+function printNTimes(message, times) {
+	for (let i = 0; i < times; i++) {
+		console.log(message);
+	}
+}
